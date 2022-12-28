c69da211ec520b26675e1ec0a7f8cd9f5b20063b
kostas214pro <kdoukas2006@gmail.com>
2022-12-28 10:04:26 +0200

    Built the main server

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
+import string
+
 from flask import Flask,send_file,request
 from pytube import YouTube
+import os
+
+path = './Songs'
+translation_table = str.maketrans('', '', string.punctuation)
+
 app = Flask(__name__)
+
+
 
 @app.route('/',methods=['GET'])
 def Main_Page():
-    idk = '47JKU1sYFzw'
+
     link = request.args.get('link')
-    youtubeLink = "https://www.youtube.com/watch?v=" + link
+    youtubeLinkTemplate = "https://www.youtube.com/watch?v="
+
+    youtubeLink = youtubeLinkTemplate + link
+    print(youtubeLink)
     yt = YouTube(youtubeLink)
     ys = yt.streams.get_audio_only()
-    ys.download()
-    fileLocation= ys.get_file_path()
+
+    safeString = yt.title
+    safeString = safeString.translate(translation_table)
+    filename = safeString.replace(" ","") + ".aac"
+
+    print("hi")
+    ys.download(filename=filename,output_path=path,skip_existing=True)
+    print(ys.get_file_path())
 
 
-
-    return send_file(fileLocation, as_attachment=True)
-
-
-
-
-
-
-
-
-
-
-
+    fileLocation = f"{path}/{filename}"
+    print(fileLocation)
+    print(filename)
+    fileToBeSend = (send_file(fileLocation, as_attachment=True))
+    return fileToBeSend
 
 
 
 
 if __name__=='__main__':
-    app.run(debug=True)
+
+    if not (os.path.exists(path)):
+        print("Created Songs directory")
+        os.mkdir(path)
+
+    app.run(debug=True,host="0.0.0.0")
